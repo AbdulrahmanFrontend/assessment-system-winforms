0e83dd02023897cc9769ac88ec804a82d2af7325
AbdulrahmanFrontend <abdulrahman.abdelstar@gmail.com>
2026-03-11 17:52:06 +0200

    Create Empty BLL, DAL, Core projects

---
+++ assessment-system-winforms.BLL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assessment_system_winforms.BLL
+{
+    public class Class1
+    {
+    }
+}
